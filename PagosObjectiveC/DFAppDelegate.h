c8c4fbe00ddf68eb9f3e64c9f5f9d03ea398bb7d
DFectuoso <santiago1717@gmail.com>
2014-07-05 12:34:10 -0500

    Initial Commit

---
+++ PagosObjectiveC/DFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DFAppDelegate.h
+//  PagosObjectiveC
+//
+//  Created by Santiago Zavala de la Vega on 7/5/14.
+//  Copyright (c) 2014 Dfectuoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PagosObjectiveC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PagosObjectiveC
+//
+//  Created by Santiago Zavala de la Vega on 7/5/14.
+//  Copyright (c) 2014 Dfectuoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DFAppDelegate class]));
+    }
+}
